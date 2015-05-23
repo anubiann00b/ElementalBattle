59ab54681740f7236ae59747359ad7c86c4a341e
Shreyas <skraman1999@gmail.com>
2015-05-23 15:38:22 -0700

    Style issues.

--- android/src/me/shreyasr/elemental/android/AndroidLauncher.java
+++ android/src/me/shreyasr/elemental/android/AndroidLauncher.java
@@ -9,7 +9,7 @@ import me.shreyasr.elemental.ElementalBattle;
 public class AndroidLauncher extends AndroidApplication {
     
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         initialize(new ElementalBattle(), config);
---
+++ core/src/me/shreyasr/elemental/board/Board.java
@@ -0,0 +1,6 @@
+package me.shreyasr.elemental.board;
+
+public class Board {
+
+    
+}
