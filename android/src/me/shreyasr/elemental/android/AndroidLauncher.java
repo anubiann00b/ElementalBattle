fe2659125bc3c86a8aa0aef76072640b189d080a
Shreyas <skraman1999@gmail.com>
2015-05-23 15:28:07 -0700

    Indent with spaces.

--- android/src/me/shreyasr/elemental/android/AndroidLauncher.java
+++ android/src/me/shreyasr/elemental/android/AndroidLauncher.java
@@ -7,10 +7,11 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import me.shreyasr.elemental.ElementalBattle;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new ElementalBattle(), config);
-	}
+    
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new ElementalBattle(), config);
+    }
 }
