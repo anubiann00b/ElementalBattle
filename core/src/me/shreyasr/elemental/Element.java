3c62616d6a76672397316208172da17c348e9278
Benjamin Yin <fellfromatree@gmail.com>
2015-05-23 17:23:01 -0700

    let's try that again

--- core/src/me/shreyasr/elemental/Element.java
+++ core/src/me/shreyasr/elemental/Element.java
@@ -4,4 +4,11 @@ package me.shreyasr.elemental;
  * Created by Benjamin on 5/23/2015.
  */
 public enum Element {
+    FIRE,
+    WATER,
+    ICE,
+    LIGHTNING,
+    EARTH,
+    HOLY,
+    ARCANE
 }
