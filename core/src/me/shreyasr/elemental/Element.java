84c15dc2a7b055d638d6f52a2daf6eee60db3454
Benjamin Yin <fellfromatree@gmail.com>
2015-05-23 21:04:20 -0700

    Counter checking

--- core/src/me/shreyasr/elemental/Element.java
+++ core/src/me/shreyasr/elemental/Element.java
@@ -4,11 +4,16 @@ package me.shreyasr.elemental;
  * Created by Benjamin on 5/23/2015.
  */
 public enum Element {
-    FIRE,
     WATER,
+    FIRE,
     ICE,
-    LIGHTNING,
     EARTH,
+    LIGHTNING,
     HOLY,
     ARCANE
+    public boolean counter(Element e){
+        if(this == HOLY || this==ARCANE || e == HOLY || e==ARCANE)
+            return false;
+        return this.ordinal() - e.ordinal() == -1;
+    }
 }
