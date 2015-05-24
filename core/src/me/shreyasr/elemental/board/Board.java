38645c1ce26115e01b1c8b76a9ceb13ee9357e1b
Benjamin Yin <fellfromatree@gmail.com>
2015-05-23 17:30:16 -0700

    Detection of links

--- core/src/me/shreyasr/elemental/board/Board.java
+++ core/src/me/shreyasr/elemental/board/Board.java
@@ -1,6 +1,43 @@
 package me.shreyasr.elemental.board;
 
-public class Board {
+import java.util.LinkedList;
 
-    //El[][] grid = new Orb[][];
+import me.shreyasr.elemental.Element;
+
+public class Board {
+    public Element[][] grid;
+    public void generate(){
+        LinkedList<Link> links = new LinkedList<Link>();
+        for(int i = 0; i < grid.length; i++){
+            int totalCount = 1;
+            Element type = grid[i][0];
+            for(int j = 1; j < grid[i].length; j++){
+                if(grid[i][j] == type){
+                    totalCount++;
+                }else{
+                    links.add(new Link(i,j,i,j-totalCount+1));
+                }
+            }
+        }
+        for(int i = 0; i < grid[0].length; i++){
+            int totalCount = 1;
+            Element type = grid[0][i];
+            for(int j = 1; j < grid.length; j++){
+                if(grid[j][i] == type){
+                    totalCount++;
+                }else{
+                    links.add(new Link(j,i,j-totalCount+1,i));
+                }
+            }
+        }
+    }
+    public class Link{
+        public int x1,y1,x2,y2;
+        public Link(int x1,int y1,int x2,int y2){
+            this.x1=x1;
+            this.y1=y1;
+            this.x2=x2;
+            this.y2=y2;
+        }
+    }
 }
