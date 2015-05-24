d8b82f14c81b4a0d5aee840ac94d555b4adbbdca
Shreyas <skraman1999@gmail.com>
2015-05-24 08:05:21 -0700

    Worst mutex NA.

--- core/src/me/shreyasr/elemental/field/Lane.java
+++ core/src/me/shreyasr/elemental/field/Lane.java
@@ -2,16 +2,17 @@ package me.shreyasr.elemental.field;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import me.shreyasr.elemental.Game;
 import me.shreyasr.elemental.field.entities.Monster;
 
 public class Lane {
 
-    public List<Monster> monsters = new CopyOnWriteArrayList<Monster>();
+    public List<Monster> monsters = new ArrayList<Monster>();
+    boolean inUse = false;
     public final int laneNum;
 
     public Lane(int laneNum) {
@@ -19,6 +20,7 @@ public class Lane {
     }
 
     public void addMonster(Monster m) {
+        while(inUse);
         monsters.add(m);
     }
 
@@ -37,9 +39,11 @@ public class Lane {
     }
 
     public void render(SpriteBatch batch, int lanePos) {
+        inUse = true;
         for (Monster m : monsters) {
             batch.draw(m.sprite.sprite,//m.sprite.animation.getKeyFrame(Game.DELTA),
                     lanePos+m.xOff-Game.LANE_WIDTH/2, (float) ((m.y+1)*Game.LANE_LENGTH+Game.LANE_START));
         }
+        inUse = false;
     }
 }
