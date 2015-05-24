a11eed7c107b226672d5b43559eeab472b3b7054
Benjamin Yin <fellfromatree@gmail.com>
2015-05-23 21:30:51 -0700

    bug with counters

--- core/src/me/shreyasr/elemental/Element.java
+++ core/src/me/shreyasr/elemental/Element.java
@@ -11,9 +11,17 @@ public enum Element {
     LIGHTNING,
     HOLY,
     ARCANE;
+
+    /**
+     * Checks counters
+     * @param e element to check against
+     * @return true if this type counters e.
+     */
     public boolean counter(Element e){
         if(this == HOLY || this==ARCANE || e == HOLY || e==ARCANE)
             return false;
+        if(this == LIGHTNING)
+            return e == WATER;
         return this.ordinal() - e.ordinal() == -1;
     }
 }
