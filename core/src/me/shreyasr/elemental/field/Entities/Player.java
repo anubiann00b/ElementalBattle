75b7fcabf3df6e9d70175cd83559dcfd98ebb290
Benjamin Yin <fellfromatree@gmail.com>
2015-05-23 21:13:18 -0700

    Added player entity

---
+++ core/src/me/shreyasr/elemental/field/Entities/Player.java
@@ -0,0 +1,12 @@
+package me.shreyasr.elemental.field.entities;
+
+/**
+ * Created by Benjamin on 5/23/2015.
+ */
+public class Player implements Entity{
+    public double health;
+    @Override
+    public void takeDamage(Attack a){
+        health-=a.baseDamage;
+    }
+}
